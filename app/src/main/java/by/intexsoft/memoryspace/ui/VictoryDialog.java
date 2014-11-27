2606f53474e78b20774d0173668266980b5533a3
Tesla <nova888411@gmail.com>
2014-11-27 10:46:38 +0400

    add custom dialogFragment for playScreen

--- app/src/main/java/by/intexsoft/memoryspace/ui/VictoryDialog.java
+++ app/src/main/java/by/intexsoft/memoryspace/ui/VictoryDialog.java
@@ -7,12 +7,22 @@ import android.app.LoaderManager;
 import android.content.DialogInterface;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EFragment;
+
+import by.intexsoft.memoryspace.R;
 import by.intexsoft.memoryspace.util.ActivityRunner;
 
 /**
  * Created by Евгений on 25.11.2014.
  */
+
 public class VictoryDialog extends DialogFragment
 {
     private SoundPool soundPool;
@@ -28,28 +38,39 @@ public class VictoryDialog extends DialogFragment
         this.loaderCallbacks = loaderCallbacks;
     }
 
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder adb = new AlertDialog.Builder(getActivity())
-                .setTitle("Победа!")
-                .setNegativeButton("Перейти в меню", new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                        soundPool.stop(streamId);
-                        getActivity().finish();
-                    }
-                })
-                .setPositiveButton("Заново", new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                        soundPool.stop(streamId);
-                        loaderManager.restartLoader(0, null,loaderCallbacks);
-                    }
-                })
-                .setMessage("Вы победили!");
-        return adb.create();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        View v = inflater.inflate(R.layout.victory_dialog_fragment, null);
+        v.findViewById(R.id.imageRepeatGame).setOnClickListener(repeatClick);
+        v.findViewById(R.id.imageBackMenu).setOnClickListener(menuClick);
+        return v;
     }
+
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+    }
+
+    public void onCancel(DialogInterface dialog) {
+        super.onCancel(dialog);
+    }
+
+    View.OnClickListener repeatClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            soundPool.stop(streamId);
+            loaderManager.restartLoader(0, null,loaderCallbacks);
+            dismiss();
+        }
+    };
+
+    View.OnClickListener menuClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            soundPool.stop(streamId);
+            soundPool.release();
+            getActivity().finish();
+            dismiss();
+        }
+    };
 }
